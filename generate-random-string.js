fd670cf71917c0ab3fecac81a76b1b8abeaba3d8
Vibesakash <akash.saini@vibescom.in>
2023-09-14 12:22:22 +0530

    generate random string

--- generate-random-string.js
+++ generate-random-string.js
@@ -1,2 +1,12 @@
 const newString = Math.random().toString(16).slice(2)
 console.log(newString)
+
+// generate string for the given length 
+const fixedStringGeneration= (length=24)=>{
+    let result= '';
+    while(result.length<length){
+      result+=Math.random().toString(36).slice(2)
+    }
+    return result.slice(0,length)
+  }
+  console.log(fixedStringGeneration())
