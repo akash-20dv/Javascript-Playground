94d321ab5d14b8ebc206226a72741afa7bc58a52
Vibesakash <akash.saini@vibescom.in>
2023-09-15 14:24:57 +0530

    pick a random number from an Array

---
+++ pick-random-number.js
@@ -0,0 +1,6 @@
+// pick a random number from an Array..
+const randomNumber = array=>  
+array[Math.floor(Math.random() * array.length)]
+    
+    console.log(randomNumber([3,5,6,7,,23,67,86]))
+  
